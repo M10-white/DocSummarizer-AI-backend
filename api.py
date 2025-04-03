919358c603313a0fa6529c94626952f217027853
Brahim CHAOUKI <bchaouki00@gmail.com>
2025-04-03 11:33:30 +0200

    fix: url repo in cors

--- api.py
+++ api.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://m10-white.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
